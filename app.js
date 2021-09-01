cccdccd66178f849ed50404416e883cb7110fcf2
Chris Chen <ok3201123@gmail.com>
2021-09-02 00:03:45 +0800

    feat: add handlebars view engine

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+)
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.render('index')
 })
 
 app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
