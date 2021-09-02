31061e2e9844db975ac81ec96c6cc960d86c5e85
Chris Chen <ok3201123@gmail.com>
2021-09-02 23:01:29 +0800

    feat: add trash talk feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+const generateTrashTalk = require('./util/generateTrashTalk.js')
 
 const PORT = process.env.PORT || 3000
 
@@ -12,9 +13,17 @@ app.engine(
   })
 )
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const { role } = req.body
+  if (!role) return res.render('index', { unselect: true })
+  const trashTalk = generateTrashTalk(role)
+  return res.render('index', { trashTalk })
 })
 
 app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
